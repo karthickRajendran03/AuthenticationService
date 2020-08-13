a28b87e2ebe1b5c47a843ef36eb813177de3052f
KarthickRajendran03 <karthick.sacs@gmail.com>
2020-08-13 22:28:38 +0530

    health

--- src/main/java/com/cognizant/emergencyHelpline/controller/HealthEndpoint.java
+++ src/main/java/com/cognizant/emergencyHelpline/controller/HealthEndpoint.java
@@ -1,4 +1,4 @@
-package com.cognizant.emergencyHelpline.controllers;
+package com.cognizant.emergencyHelpline.controller;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
